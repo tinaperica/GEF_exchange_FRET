8a57e36b6b89e8e6be80a0c755637ab94192aa8f
tinaperica <tina.perica@gmail.com>
2016-08-29 14:42:52 -0700

    This code can now handle multiple experiments of identical conditions (same Gsp1 and GEF conc, but performed on different days)

---
+++ Plot_FRETkin_individual_experiment.R
@@ -0,0 +1,72 @@
+library(ggplot2)
+max.na.rm <- function (x) {
+  max(x, na.rm = T)
+}
+min.na.rm <- function (x) {
+  min(x, na.rm = T)
+}
+setwd("~/Documents/GSP1_experimental_data/GEF_exchange_exp/")
+tecan<-read.delim("data/20160824_FRET_kinetics3_parsed.txt", stringsAsFactors = F, head = F)
+names(tecan) <- c("exp_date", "time", "row", "col", "protein", "condition", "Gsp1_concentration", "GEF_concentration", "fluorescence")
+head(tecan)
+tecan <- cbind(tecan,paste(tecan[[8]], "nM", sep = " "))
+names(tecan)[length(names(tecan))] <- "GEF_conc_condition"
+tecan <- tecan[complete.cases(tecan),]
+tecan <- subset(tecan, GEF_concentration > 0)
+name_prefix <- Sys.Date()
+rawoutfilename = paste (name_prefix, "_FRET_kinetics_raw.pdf", collapse = "")
+normoutfilename = paste(name_prefix, "_FRET_kinetics_norm.pdf", collapse = "")
+scaledoutfilename = paste(name_prefix, "_FRET_kinetics_scaled.pdf", collapse = "")
+proteins<-unique(tecan$protein)
+
+tecan.start.max <- with(tecan, aggregate(fluorescence, by = list(protein = protein, condition = condition), max.na.rm))
+tecan.start.min <- with(tecan, aggregate(fluorescence, by = list(protein = protein, condition = condition), min.na.rm))
+tecan.normalized.max<-merge(tecan, tecan.start.max, by = c("protein", "condition"))
+tecan.normalized <- merge(tecan.normalized.max, tecan.start.min, by = c("protein", "condition"))
+names(tecan.normalized)[c(length(names(tecan.normalized)) - 1, length(names(tecan.normalized)))] <- c("max", "min") 
+head(tecan.normalized)
+tecan.normalized$normalized <- 1 - (tecan.normalized$max - tecan.normalized$fluorescence)/(tecan.normalized$max - tecan.normalized$min)
+tecan.normalized$scaled <- (tecan.normalized$fluorescence - tecan.normalized$max)
+tecan.normalized <- tecan.normalized[order(tecan.normalized$protein, tecan.normalized$Gsp1_concentration, tecan.normalized$time),]
+
+
+plots<-list()
+for (i in 1:length(proteins)) {
+  prot <- proteins[i]
+  temp<-subset(tecan, protein == prot)
+  temp <- temp[order(temp$time),]
+  plots[[i]] <- ggplot(data=temp, aes(x = time, y = fluorescence, group = interaction(condition, GEF_conc_condition), colour = condition, shape = GEF_conc_condition)) + geom_point(size = 0.7) #+ geom_line()
+  plots[[i]] <- plots[[i]] + ggtitle(prot)
+  # for (j in 1:length(unique(temp$Gsp1_concentration))) {
+  #   conc <- unique(temp$Gsp1_concentration)[j]
+  #   temp.conc <- subset(temp, Gsp1_concentration == conc)
+  #   temp.conc <- temp.conc[order(temp.conc$time),]
+  # }
+}
+pdf(file = rawoutfilename)
+bquiet<-lapply(plots, print)
+dev.off()
+plots<-list()
+for (i in 1:length(proteins)) {
+  prot <- proteins[i]
+  temp<-subset(tecan.normalized, protein == prot)
+  temp <- temp[order(temp$time),]
+  plots[[i]]<-ggplot(data=temp, aes(x = time, y = normalized, group = interaction(condition, GEF_conc_condition), colour = condition, shape = GEF_conc_condition)) + geom_point()
+  #plots[[i]]<-plots[[i]] + stat_smooth(span = 0.5)
+  plots[[i]]<-plots[[i]] + ggtitle(prot)
+}
+pdf(file = normoutfilename)
+bquiet<-lapply(plots, print)
+dev.off()
+plots<-list()
+for (i in 1:length(proteins)) {
+  prot <- proteins[i]
+  temp<-subset(tecan.normalized, protein == prot)
+  temp <- temp[order(temp$time),]
+  plots[[i]]<-ggplot(data=temp, aes(x = time, y = scaled, group = interaction(condition, GEF_conc_condition), colour = condition, shape = GEF_conc_condition)) + geom_point()
+  #plots[[i]]<-plots[[i]] + stat_smooth(span = 0.5)
+  plots[[i]]<-plots[[i]] + ggtitle(prot)
+}
+pdf(file = scaledoutfilename)
+bquiet<-lapply(plots, print)
+dev.off()
--- fit_and_plot_kinetics_and_MM.R
+++ fit_and_plot_kinetics_and_MM.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 library(minpack.lm)
-setwd("~/Documents/GSP1_data/GEF_exchange_exp/")
+setwd("~/Documents/GSP1_experimental_data/GEF_exchange_exp/")
 outsufix <- paste("_fitted_curves_", Sys.Date(), ".pdf", sep = "")
 tecan<-read.delim("data/FRET_kinetics.txt", stringsAsFactors = F, head = F)
-### the file with exp to discard was manually made based on raw plots
+### the file with exp to discard was manually made based on inspection of all the raw data plots
 data.to.discard <- read.delim("data/FRET_kinetics_data_to_discard.txt", head = T) ### defined as date of exp, prot, prot conc, and GEF conc
 #####
 names(tecan) <- c("exp_date", "time", "row", "col", "protein", "condition", "Gsp1_concentration", "GEF_concentration", "fluorescence")
@@ -13,15 +13,13 @@ names(tecan)[length(names(tecan))] <- "GEF_conc_condition"
 tecan <- tecan[complete.cases(tecan),]
 tecan <- subset(tecan, GEF_concentration > 0)
 tecan<-cbind(tecan, paste(tecan$exp_date, tecan$protein, tecan$Gsp1_concentration, tecan$GEF_concentration, sep = "_"))
-names(tecan)[length(names(tecan))] <- "unique"
+names(tecan)[length(names(tecan))] <- "unique"  #### unique is a combination of experiment date and conditions (Ran and GEF concentrations)
 data.to.discard <- cbind(data.to.discard, paste(data.to.discard$exp_date, data.to.discard$protein, data.to.discard$Gsp1_concentration, data.to.discard$GEF_concentration, sep = "_"))
 names(data.to.discard)[length(names(data.to.discard))] <- "unique"
 unique_identifiers_to_discard <- as.character(data.to.discard$unique)
 tecan.discarded <- tecan[tecan$unique %in% unique_identifiers_to_discard,]
 tecan <- tecan[! tecan$unique %in% unique_identifiers_to_discard,]
 proteins<-unique(tecan$protein)
-#proteins <- proteins[proteins != "PE7_Q147E"]
-#proteins <- c("PE1_WT", "PE2_T34A")
 
 get.initial.rate.subset <- function (total, conc) {
   min.total.fluor <- quantile(total$fluorescence, 0.001)
@@ -30,17 +28,19 @@ get.initial.rate.subset <- function (total, conc) {
   total <- cbind(total, "substrate_conc" = conc - conc*((max.total.fluor - total$fluorescence)/(max.total.fluor - min.total.fluor)))
   cutoff.rxn.start.time <- numeric()
   if (conc < 1) {
-    cutoff.rxn.start.time <- max(total$time[total$percent.norm.fluorescence < 0.4])
-  } else {
     cutoff.rxn.start.time <- max(total$time[total$percent.norm.fluorescence < 0.5])
+    #cutoff.rxn.start.time <- max(total$time[total$percent.norm.fluorescence < 0.9])
+  } else {
+    cutoff.rxn.start.time <- max(total$time[total$percent.norm.fluorescence < 0.6])  # 0.8
+    #cutoff.rxn.start.time <- max(total$time[total$percent.norm.fluorescence < 0.9])
   }
   cutoff.rxn.start.percent <- subset(total, time < cutoff.rxn.start.time)
   return(cutoff.rxn.start.percent)
 }
-fit_rate_and_plot <- function (initial, prot, conc, GEF_conc) {
+fit_rate_and_plot <- function (initial, prot, conc, GEF_conc, exp) {
   time <- initial$time
   getPred <- function(pars, xx) pars$f0 * exp(- pars$k * time) + pars$c
-  plot(initial$time, initial$substrate_conc, xlab = "time / s", ylab = "substrate concentration", main = paste(prot, conc, GEF_conc, collapse = " "))
+  plot(initial$time, initial$substrate_conc, xlab = "time / s", ylab = "substrate concentration", main = paste(prot, conc, GEF_conc, exp, collapse = " "))
   residFun <- function(p, observed, xx) observed - getPred(p,xx)
   parStart <- list(f0 = conc, k = 0.01, c = conc/2)
   nls.out <- nls.lm(par = parStart, fn = residFun, observed = initial$substrate_conc, xx = time, control = nls.lm.control(nprint = 1))
@@ -50,27 +50,27 @@ fit_rate_and_plot <- function (initial, prot, conc, GEF_conc) {
   k <- coef(nls.out)[2]
   f0 <- coef(nls.out)[1]
   v0 = (f0 * k * exp(k * 0)) / (GEF_conc*0.001)
-  return(data.frame(prot, conc, f0, k, v0, GEF_conc))
+  return(data.frame(prot, conc, f0, k, v0, GEF_conc, exp))
 }
 
-# fit_MM <- function (initial_vs_conc, prot) {
-#   test.conc <- seq(min(initial_vs_conc$conc), max(initial_vs_conc$conc), 0.25)
-#   conc <- initial_vs_conc$conc
-#   getFit <- function(pars, xx) conc * pars$Vmax / (pars$Km + conc)
-#   residFun <- function(p, observed, xx) observed - getFit(p,xx)
-#   Km.estimate <- 1
-#   Vm.estimate <- max(initial_vs_conc$v0)
-#   parStart <- list(Vmax = Vm.estimate, Km = Km.estimate)
-#   nls.out <- nls.lm(par = parStart, fn = residFun, observed = initial_vs_conc$v0, xx = conc, control = nls.lm.control(nprint = 1))
-#   getPred <- function(pars, xx) test.conc * pars$Vmax / (pars$Km + test.conc)
-#   predicted.line <- getPred(as.list(coef(nls.out)), test.conc)
-#   predicted.line.data.frame <- data.frame(conc = test.conc, v0 = predicted.line, prot = prot)
-#   Vmax <- coef(nls.out)[1]
-#   Km <- coef(nls.out)[2]
-#   kcat = Vmax
-#   parameters_and_predicted_line <- list("parameters" = data.frame(prot, Vmax, Km, kcat), "predicted_line_df" = predicted.line.data.frame)
-#   return(parameters_and_predicted_line)
-# }
+fit_MM <- function (initial_vs_conc, prot) {
+  test.conc <- seq(min(initial_vs_conc$conc), max(initial_vs_conc$conc), 0.25)
+  conc <- initial_vs_conc$conc
+  getFit <- function(pars, xx) conc * pars$Vmax / (pars$Km + conc)
+  residFun <- function(p, observed, xx) observed - getFit(p,xx)
+  Km.estimate <- 1
+  Vm.estimate <- max(initial_vs_conc$v0)
+  parStart <- list(Vmax = Vm.estimate, Km = Km.estimate)
+  nls.out <- nls.lm(par = parStart, fn = residFun, observed = initial_vs_conc$v0, xx = conc, control = nls.lm.control(nprint = 1))
+  getPred <- function(pars, xx) test.conc * pars$Vmax / (pars$Km + test.conc)
+  predicted.line <- getPred(as.list(coef(nls.out)), test.conc)
+  predicted.line.data.frame <- data.frame(conc = test.conc, v0 = predicted.line, prot = prot)
+  Vmax <- coef(nls.out)[1]
+  Km <- coef(nls.out)[2]
+  kcat = Vmax
+  parameters_and_predicted_line <- list("parameters" = data.frame(prot, Vmax, Km, kcat), "predicted_line_df" = predicted.line.data.frame)
+  return(parameters_and_predicted_line)
+}
 
 
 fitting.parameters <- data.frame()
@@ -81,9 +81,12 @@ for (prot in proteins) {
   for (conc in substr.concentrations) {
     GEF_concentrations <- unique(tecan$GEF_concentration[tecan$protein == prot & tecan$Gsp1_concentration == conc])
     for (GEF_conc in GEF_concentrations) {
-      data.subset <- subset(tecan, protein == prot & Gsp1_concentration == conc & GEF_concentration == GEF_conc, select=c(time = time, fluorescence = fluorescence))
-      relevant.subset <- get.initial.rate.subset(data.subset, conc)
-      fitting.parameters <- rbind(fitting.parameters, fit_rate_and_plot(relevant.subset, prot, conc, GEF_conc))
+      experiments <- unique(tecan$exp_date[tecan$protein == prot & tecan$Gsp1_concentration == conc & tecan$GEF_concentration == GEF_conc])
+      for (exp in experiments) {
+        data.subset <- subset(tecan, protein == prot & Gsp1_concentration == conc & GEF_concentration == GEF_conc & tecan$exp_date == exp, select=c(time = time, fluorescence = fluorescence))
+        relevant.subset <- get.initial.rate.subset(data.subset, conc)
+        fitting.parameters <- rbind(fitting.parameters, fit_rate_and_plot(relevant.subset, prot, conc, GEF_conc, exp))
+      }
     }
   }
   dev.off()
@@ -97,7 +100,7 @@ KmVm.curve.to.plot <- data.frame()
 Kmkcat.df <- data.frame()
 for (i in 1:length(proteins)) {
   mutant = proteins[i]
-  data.subset <- subset(fitting.parameters, prot == mutant, select=c(conc = conc, v0 = v0, GEF_conc = GEF_conc))
+  data.subset <- subset(fitting.parameters, prot == mutant, select=c(conc = conc, v0 = v0, GEF_conc = GEF_conc, exp = exp))
   MM.parameters[i] <- list (mutant = fit_MM(data.subset, mutant))
   KmVm.param.df <- rbind(KmVm.param.df, MM.parameters[[i]]$parameters)
   KmVm.curve.to.plot <- rbind(KmVm.curve.to.plot, MM.parameters[[i]]$predicted_line_df)
