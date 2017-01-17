39aa145e10a859c0b8966d5c52f05c144753e02a
tinaperica <tina.perica@gmail.com>
2017-01-17 12:57:48 -0800

    discrets test

--- GTPase_assay/plot_individual_GAP_assay_experiments.R
+++ GTPase_assay/plot_individual_GAP_assay_experiments.R
@@ -1,6 +1,6 @@
 ### plot and analyse GTPase assay with phosphate sensor data
 options( stringsAsFactors = F )
-data <- read.delim("GTPase_assay/data/20170113_GAP_kinetics_parsed.txt", head = F)
+data <- read.delim("GTPase_assay/data/20170110_GAP_kinetics_parsed.txt", head = F)
 names(data) <- expression( date, time, row, column, sample, conc_string, conc, GAP_conc, fluorescence)
 head(data)
 data$prot_conc <- do.call(paste, c(data[, c("sample", "conc_string", "GAP_conc")], sep = " "))
@@ -14,3 +14,11 @@ for (i in seq_along(samples)) {
 }
 legend("top", legend = col_list[[1]], title = "Ran conc / uM and GAP conc / nM", fill = col_list[[2]], bty = "n", cex = 0.6)
 
+# predict.lo <- loess(fluorescence ~ time, temp, span = 0.3)
+# time <- seq(0, 3000, 10)
+# predicted.curve <- predict(predict.lo, time)
+# plot(temp$time, temp$fluorescence)
+# lines(time, predicted.curve, col = "red")
+# discrets(na.omit(predict(predict.lo, time)), sym = 3)
+# 
+# discrets(na.omit(temp$fluorescence), sym = 3)
